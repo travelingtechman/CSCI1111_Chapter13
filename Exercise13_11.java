21fe1e7e5187389add655e171d46763de0a7e864
travelingtechman <53628990+travelingtechman@users.noreply.github.com>
2022-06-15 12:36:24 -0600

    Update Exercise13_11.java
    added required code

--- Exercise13_11.java
+++ Exercise13_11.java
@@ -3,7 +3,14 @@ package chapter_13;
 public class Exercise13_11 {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		
+		Octagon oct1 = new Octagon(15.0);
+		
+		Octagon oct2 = (Octagon) oct1.clone();
+		//System.out.println(oct3.getSide());
+		
+		System.out.println(oct1.compareTo(oct2));
 	}
 }
+
